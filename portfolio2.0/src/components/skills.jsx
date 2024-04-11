6d92b779ac05c101781a35513d3ff742f4580039
dheerajnandiraju@gmail.com <dheerajnandiraju@gmail.com>
2024-04-11 20:10:35 +0530

    skills working on

--- portfolio2.0/src/components/skills.jsx
+++ portfolio2.0/src/components/skills.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./skills.css";
 
 function Skills() {
   return (
-    <div>Skills</div>
-  )
+    <div>
+      <heading id="head">Skills</heading>
+      <div className="main">
+        <div className="container">
+          <div className="sideheading">: : Soft Skill</div>
+          <div className="container2">
+            <div className="skills">Communication</div>
+            <div className="skills">Comm</div>
+            <div className="skills">Communication</div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="sideheading">: : Soft Skill</div>
+        </div>
+        <div className="container">
+          <div className="sideheading">: : Soft Skill</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
