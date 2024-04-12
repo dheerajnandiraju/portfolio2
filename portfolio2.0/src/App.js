b13ef506cbe81722696d13481faf79a8db431f92
dheerajnandiraju@gmail.com <dheerajnandiraju@gmail.com>
2024-04-12 13:49:53 +0530

    skills done

--- portfolio2.0/src/App.js
+++ portfolio2.0/src/App.js
@@ -51,6 +51,8 @@ window.removeEventListener("mousemove", mouseMove)
 <About/>
 <hr/>
 <Skills/>
+<hr/>
+
     </div>
   );
 }
--- portfolio2.0/src/components/skills.jsx
+++ portfolio2.0/src/components/skills.jsx
@@ -3,22 +3,47 @@ import "./skills.css";
 
 function Skills() {
   return (
-    <div>
+    <div style={{ verticalAlign: "center" }}>
       <heading id="head">Skills</heading>
       <div className="main">
-        <div className="container">
+        <div style={{ margin: "0rem 0rem 0rem 4vw" }} className="container">
           <div className="sideheading">: : Soft Skill</div>
           <div className="container2">
             <div className="skills">Communication</div>
-            <div className="skills">Comm</div>
-            <div className="skills">Communication</div>
+            <div className="skills">Adaptivity</div>
+            <div className="skills">Team work</div>
+            <div className="skills">Critical Thinking</div>
+            <div className="skills">Time Management</div>
+            <br />
+            <div className="skills">Self-Motivation</div>
+            <div className="skills">Learninf Agility</div>
           </div>
         </div>
         <div className="container">
-          <div className="sideheading">: : Soft Skill</div>
+          <div className="sideheading">: : Technical Skills</div>
+          <div className="container2">
+            <div className="skills">Front-end Development</div>
+            <div className="skills">Version Control</div>
+            <div className="skills">Design Software</div>
+            <div className="skills">Design Principles</div>
+            <br />
+            <div className="skills">Programming</div>
+            <div className="skills">API Integrating</div>
+          </div>
         </div>
         <div className="container">
-          <div className="sideheading">: : Soft Skill</div>
+          <div className="sideheading">: : Tools</div>
+          <div className="container2">
+            <div className="skills">Figma</div>
+            <div className="skills">Adobe Lightroom</div>
+            <br />
+            <div className="skills">Adobe Photoshop</div>
+            <div className="skills">Git/GitHub</div>
+            <div className="skills">Canva</div>
+            <br />
+
+            <div className="skills">VS Code</div>
+          </div>
         </div>
       </div>
     </div>
